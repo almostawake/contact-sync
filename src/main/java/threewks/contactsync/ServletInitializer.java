e0707acd4dffdc10663e13575fe3498a353ee525
Dave Kuhn <david.s.kuhn@gmail.com>
2017-11-30 15:09:17 +1100

    Setup spring profiles for per-environment config

--- src/main/java/threewks/contactsync/ServletInitializer.java
+++ src/main/java/threewks/contactsync/ServletInitializer.java
@@ -1,11 +1,28 @@
 package threewks.contactsync;
 
+import contrib.springframework.data.gcp.config.helper.ProfileExtractors;
+import contrib.springframework.data.gcp.config.helper.ProfileResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+import java.util.List;
+
 public class ServletInitializer extends SpringBootServletInitializer {
+    private static final Logger LOG = LoggerFactory.getLogger(ServletInitializer.class);
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
+        List<String> profiles = new ProfileResolver()
+            // Allow a profile if we want to set properties for all gae environments
+            .setAdditionalProfileExtractor(ProfileExtractors.staticValue("gae"))
+            // Allow us to reference "dev", "uat" without full app id
+            .setAdditionalProfileExtractor(ProfileExtractors.AFTER_LAST_DASH)
+            .getProfiles();
+        LOG.info("Setting profiles: {}", profiles);
+
+        return application.sources(Application.class)
+            .profiles(profiles.toArray(new String[profiles.size()]));
     }
 }
